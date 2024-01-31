bb35a5f33eff1c54b501d649ac6b7bbf2186750d
Jie Qiu <jieq@jies-mbp.lan>
2024-01-30 16:14:07 -0800

    test

--- apollo/src/datasources/odds-api.js
+++ apollo/src/datasources/odds-api.js
@@ -1,6 +1,11 @@
 import { RESTDataSource } from "@apollo/datasource-rest"
+import dotenv, { config } from "dotenv"
 
 const myAPIKey = "fa5025a819281e91a5e3cadc2045d1ae"
+
+// dotenv.config({ path: "../../../.env" })
+// const myAPIKey = process.env.API_KEY
+// console.log(myAPIKey)
 
 class OddsAPI extends RESTDataSource {
   baseURL = "https://api.the-odds-api.com/"
